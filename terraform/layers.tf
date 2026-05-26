34fbb27b66a705b07708599d13fec912d82aa10c
Ivan Gonzalez <igonzalez@codebranch.co>
2026-05-25 23:39:08 -0500

    feat: implement GitHub Actions workflow for Lambda layer deployment and update .gitignore

--- terraform/layers.tf
+++ terraform/layers.tf
@@ -1,25 +1,7 @@
-resource "null_resource" "install_layer" {
-  triggers = {
-    requirements = filemd5("${path.module}/../layers/requirements.txt")
-  }
-
-  provisioner "local-exec" {
-    command = "pip install -r ${path.module}/../layers/requirements.txt -t ${path.module}/../layers/python/ --quiet"
-  }
-}
-
-data "archive_file" "lambda_layer" {
-  depends_on  = [null_resource.install_layer]
-  type        = "zip"
-  source_dir  = "${path.module}/../layers"
-  output_path = "${path.module}/../builds/lambda_layer.zip"
-}
-
 resource "aws_lambda_layer_version" "deps" {
   layer_name          = "${var.project_name}-deps"
-  filename            = data.archive_file.lambda_layer.output_path
-  source_code_hash    = data.archive_file.lambda_layer.output_base64sha256
+  filename            = "${path.module}/../builds/lambda_layer.zip"
+  source_code_hash    = filebase64sha256("${path.module}/../builds/lambda_layer.zip")
   compatible_runtimes = [var.lambda_runtime]
-
-  description = "anthropic + requests para app-finanzas"
+  description         = "anthropic + requests para app-finanzas"
 }
